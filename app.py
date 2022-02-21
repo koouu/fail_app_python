8fabb25d24140d93dc7113dd233c5d645ca8d0f1
koouu <kotatata420@gmail.com>
2022-02-22 03:27:29 +0900

    login

--- app.py
+++ app.py
@@ -17,28 +17,52 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+URL = "http://localhost:1323"
+
 def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/")
-        return f(*args, **kwargs)
-    return decorated_function
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		if session.get("user_id") is None:
+			return redirect("/login")
+		return f(*args, **kwargs)
+	return decorated_function
 
 
 
 @app.after_request
 def after_request(response):
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+	"""Ensure responses aren't cached"""
+	response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	response.headers["Expires"] = 0
+	response.headers["Pragma"] = "no-cache"
+	return response
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	session.clear()
+	if request.method == "POST":
+		name=request.form.get("username")
+		password=request.form.get("password")
+		res=requests.post(URL+"/login",data={"name":name,"password":password})
+		
+		if res.json()["name"]=="":
+			return render_template("login.html")
+		return redirect("/")
+	else:
+		return render_template("login.html")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+	res = requests.get(URL+"/user")
+	print(res)
+	print(res.json())
+	jsondata=res.json()
+	print("fas:")
+	print(jsondata[0]["id"])
+	return render_template("index.html")
+
+
 
 if __name__ == '__main__':
    app.run(debug=True)
